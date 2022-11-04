d4d07cf34d1af4ea430906b998ddc01c2838675a
Alex <devstar0718@gmail.com>
2022-11-04 14:01:17 +0900

    Update title, align

--- src/components/Desc.tsx
+++ src/components/Desc.tsx
@@ -1,8 +1,8 @@
 
 export const TopDesc = () => {
     return (
-        <div className="p-4 md:-mt-24">
-            <h1 className='text-3xl font-bold text-slate-200 py-2'>@Metakey / Unique MetaKey holders</h1>
+        <div className="p-4 md:-mt-24 text-center ">
+            <h1 className='text-3xl font-bold text-slate-200 py-2'>Unique MetaKey holders</h1>
             <span className='text-slate-400'>Referred from 
                 <a className='text-blue-700' target="_blank" href='https://dune.com/queries/276792' rel="noreferrer"> Dune Analytics</a>
             </span>
