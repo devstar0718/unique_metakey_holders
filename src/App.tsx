b3bc3e9811d10e08768aad872f30474ba6fd477c
Alex <devstar0718@gmail.com>
2022-11-04 13:46:15 +0900

    initial mvp project

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './App.css';
+
+import { Chart, registerables } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
+import PieCt from './components/PieCt';
+import HBarCt from './components/HBarCt';
+import HisCt from './components/HisCt';
+import { ChartDesc, TopDesc } from './components/Desc';
+import { ChartCard } from './components/ChartCard';
+
+
+if (registerables.length > -1 )
+  Chart.register(...registerables, ChartDataLabels);
+
+function App() {
+  return (
+    <>
+      {        
+        (typeof registerables !== 'undefined') && 
+        <div className="App font-mono min-h-screen flex bg-gradient-to-r from-slate-800 to-slate-700">
+          <div className='container mx-auto flex flex-col justify-center'>
+            <TopDesc />
+            <div className='flex flex-col md:flex-row'>
+              <ChartCard className='md:w-1/5'>
+                <PieCt />
+                <ChartDesc desc="Holder Wallets" />
+              </ChartCard>
+              <ChartCard className='md:w-2/5'>
+                <HBarCt />
+                <ChartDesc desc="Unique Holders"/>
+              </ChartCard>          
+              <ChartCard className='md:w-2/5'>
+                <HisCt />            
+                <ChartDesc desc="Holder Distribution"/>
+              </ChartCard>          
+            </div>
+          </div>
+        </div>
+      }
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/HisCt.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+
+import dataJson from '../api/data.json';
+
+function HisCt() {
+  const l = dataJson.names;
+
+  const d = () => {
+    const ra : Array<number> = dataJson.distribution;
+    return ra;
+  }
+
+  const options : any = {    
+    scales: {
+      x: {
+        grid: {
+          drawBorder: false,
+          display: false,
+        }
+      },
+      y: {        
+        grid: {
+          color: '#64748b',
+          borderColor: "#e2e8f0",
+          
+        }
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },    
+      datalabels: {
+        formatter: function (v : number, c : any) {        
+          return v + '%';
+        },
+        display: true,
+        color: "white",
+        font: {
+          size: 12,
+        }
+      },
+      tooltip: {
+        callbacks: {
+          title: function() {},
+          label: (context : any) => {
+            return context.label + ' : ' + context.formattedValue + '%';
+          }
+        }
+      }
+    },
+    borderWidth: 0,
+    hoverOffset: 5,
+    offset: 2,
+  };
+
+  const data = {
+    labels: l,
+    datasets: [
+      {
+        axis: 'y',
+        label: 'Histogram',
+        data: d(),
+        backgroundColor: [
+          "#0284c7",
+          "#0369a1",
+          "#075985",
+          "#0c4a6e",
+        ],
+      },    
+    ]
+  };
+
+  return (
+    <>
+      <Bar data={data} options={options} />      
+    </>
+  );
+}
+
+export default HisCt;
