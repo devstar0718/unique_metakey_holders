220559804944895345aa84d4f44fb1ad4af0a8ff
Alex <devstar0718@gmail.com>
2022-11-04 21:01:15 +0800

    Fix reverse values

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,9 +3,13 @@ import { render, screen } from '@testing-library/react';
 import { ChartDesc } from "./components/Desc";
 // import App from './App';
 
-test('renders', () => {
-  render(<ChartDesc/>)  
-  // render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+describe('all function', () => {
+  
 });
+
+// test('renders', () => {
+//   render(<ChartDesc/>)  
+//   // render(<App />);
+//   // const linkElement = screen.getByText(/learn react/i);
+//   // expect(linkElement).toBeInTheDocument();
+// });
